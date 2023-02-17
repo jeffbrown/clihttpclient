7d5bac99badda422b1d9096fa387fa24cc31a56f
Jeff Scott Brown <jeff@unityfoundation.io>
2023-02-17 09:56:22 -0600

    initial

---
+++ src/main/java/clihttpclient/ClihttpclientCommand.java
@@ -0,0 +1,28 @@
+package clihttpclient;
+
+import io.micronaut.configuration.picocli.PicocliRunner;
+import io.micronaut.context.ApplicationContext;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "clihttpclient", description = "...",
+        mixinStandardHelpOptions = true)
+public class ClihttpclientCommand implements Runnable {
+
+    @Option(names = {"-v", "--verbose"}, description = "...")
+    boolean verbose;
+
+    public static void main(String[] args) throws Exception {
+        PicocliRunner.run(ClihttpclientCommand.class, args);
+    }
+
+    public void run() {
+        // business logic here
+        if (verbose) {
+            System.out.println("Hi!");
+        }
+    }
+}
